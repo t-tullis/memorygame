6f7831dd18e9e63332b9c3704ca8e3028953577b
t-tullis <t.tullis34@gmail.com>
2017-11-08 01:05:46 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var cards = ['queen', 'queen', 'king', 'king']
+var cardsInPlay =[];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped ' + cardOne)
+console.log('User flipped ' + cardTwo)
+
+if (cardsInPlay.length === 2){
+	if(cardOne === cardTwo){
+		alert('You found a match!');
+	}else{
+		alert('Sorry, try again');
+	}
+}
